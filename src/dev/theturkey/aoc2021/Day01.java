38e334cfc5de26699eab0f7b8f18d1ceff5d0e4c
TheTurkeyDev <turkey@theturkey.dev>
2021-12-01 16:27:36 -0500

    Cleanup and parts comments

--- src/dev/theturkey/aoc2021/Day01.java
+++ src/dev/theturkey/aoc2021/Day01.java
@@ -15,31 +15,24 @@ public class Day01 extends AOCPuzzle
 	{
 		List<Integer> depths = convertToInts(input);
 
+		//PART 1
 		int changes = 0;
 		for(int i = 1; i < depths.size(); i++)
-		{
 			if(depths.get(i - 1) < depths.get(i))
-			{
 				changes++;
-			}
-		}
 
 		lap(changes);
 
+		//PART 2
 		List<Integer> windows = new ArrayList<>();
 		for(int i = 2; i < depths.size(); i++)
-		{
 			windows.add(depths.get(i) + depths.get(i - 1) + depths.get(i - 2));
-		}
 
 		changes = 0;
 		for(int i = 1; i < windows.size(); i++)
-		{
 			if(windows.get(i - 1) < windows.get(i))
-			{
 				changes++;
-			}
-		}
+
 		lap(changes);
 	}
 }
