1df8d24edd31134b37c5a7c2c8d33beb0c3b1696
TheTurkeyDev <turkey@theturkey.dev>
2021-12-10 00:31:00 -0500

    Day 10 Code

--- src/dev/theturkey/aoc2021/AoC2021Core.java
+++ src/dev/theturkey/aoc2021/AoC2021Core.java
@@ -12,8 +12,8 @@ public class AoC2021Core
 		//new Day06();
 		//new Day07();
 		//new Day08();
-		new Day09();
-		//new Day10();
+		//new Day09();
+		new Day10();
 		//new Day11();
 		//new Day12();
 		//new Day13();
---
+++ src/dev/theturkey/aoc2021/Day10.java
@@ -0,0 +1,121 @@
+package dev.theturkey.aoc2021;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day10 extends AOCPuzzle
+{
+	public Day10()
+	{
+		super("10");
+	}
+
+	@Override
+	void solve(List<String> input)
+	{
+		//PART 1
+		long score = 0;
+		for(int i = input.size() - 1; i >= 0; i--)
+		{
+			List<Character> queue = new ArrayList<>();
+			for(char c : input.get(i).toCharArray())
+			{
+				boolean exit = false;
+				switch(c)
+				{
+					case '(':
+					case '[':
+					case '{':
+					case '<':
+						queue.add(c);
+						break;
+					case ')':
+						if(queue.remove(queue.size() - 1) != '(')
+						{
+							score += 3;
+							exit = true;
+						}
+						break;
+					case ']':
+						if(queue.remove(queue.size() - 1) != '[')
+						{
+							score += 57;
+							exit = true;
+						}
+						break;
+					case '}':
+						if(queue.remove(queue.size() - 1) != '{')
+						{
+							score += 1197;
+							exit = true;
+						}
+						break;
+					case '>':
+						if(queue.remove(queue.size() - 1) != '<')
+						{
+							score += 25137;
+							exit = true;
+						}
+						break;
+				}
+
+				if(exit)
+				{
+					input.remove(i);
+					break;
+				}
+			}
+		}
+		lap(score);
+
+		//PART 2
+		List<Long> scores = new ArrayList<>();
+		for(int i = input.size() - 1; i >= 0; i--)
+		{
+			List<Character> queue = new ArrayList<>();
+			for(char c : input.get(i).toCharArray())
+			{
+				switch(c)
+				{
+					case '(':
+					case '[':
+					case '{':
+					case '<':
+						queue.add(c);
+						break;
+					case ')':
+					case ']':
+					case '}':
+					case '>':
+						queue.remove(queue.size() - 1);
+						break;
+				}
+			}
+
+			score = 0;
+			while(queue.size() > 0)
+			{
+				char c = queue.remove(queue.size() - 1);
+				score *= 5;
+				switch(c)
+				{
+					case '(':
+						score += 1;
+						break;
+					case '[':
+						score += 2;
+						break;
+					case '{':
+						score += 3;
+						break;
+					case '<':
+						score += 4;
+						break;
+				}
+			}
+			scores.add(score);
+		}
+		scores.sort(Long::compareTo);
+		lap(scores.get(scores.size() / 2));
+	}
+}
